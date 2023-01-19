2c31ede2bf7766c00e98d105941ff34c80b66a57
AkopianEvgenii112708 <eakopian2002@mail.ru>
2023-01-19 20:32:20 +0600

    new

--- shopApi/urls.py
+++ shopApi/urls.py
@@ -23,9 +23,11 @@ from drf_yasg.views import get_schema_view
 from rest_framework.routers import SimpleRouter
 
 from category.views import CategoryViewSet
+from product.views import ProductViewSet
 
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -50,3 +52,5 @@ urlpatterns = [
     re_path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
